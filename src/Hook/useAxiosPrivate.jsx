774e6e6a220589264ae23410ef027aa9690f5b6f
Md-Mansor <mansor.me777@ gmail.com>
2023-12-17 15:44:45 +0600

    change base url

--- src/Hook/useAxiosPrivate.jsx
+++ src/Hook/useAxiosPrivate.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const secure = axios.create({
-    baseURL: "https://tech-matrix-server.vercel.app/",
+    baseURL: "https://tech-matrix-server.vercel.app",
     withCredentials: true
 })
 
--- src/Hook/useAxiosPublic.jsx
+++ src/Hook/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL: "https://tech-matrix-server.vercel.app/",
+    baseURL: "https://tech-matrix-server.vercel.app",
     withCredentials: true
 })
 
